9650f0e492ece7a395bccba4ae919be0e15f52d1
Emanuel Camarena <emanuel.ortega@hotmail.com>
2023-09-22 08:44:19 -0600

    class

--- 03-server-express.js
+++ 03-server-express.js
@@ -1,2 +1,24 @@
 import express from "express"
 
+const app = express()
+
+// req => params, query, body
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+// /api/character => buscar todos los personajes o crear un personaje
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.post("/api/character", (req, res) => {
+    res.send("Hello World")
+})
+app.get("/user", (req, res) => {
+    res.send("Hello User")
+})
+
+app.listen(3000, () => {
+    console.log("Server currently running at: http://localhost:3000")
+})
--- 03-server.js
+++ 03-server.js
@@ -3,5 +3,6 @@ import http from 'http'
 const server = http.createServer()
 
 server.listen(8080,()=>{
-    console.log("Server currrently running at: https://localhost:8080")
+    console.log("Server currrently running at: http://localhost:8080")
 })
+
